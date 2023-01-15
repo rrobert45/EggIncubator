361125b982bc5383148099e6769407424e15efe5
rrobert45 <rrobert45@gmail.com>
2023-01-14 23:01:07 -0500

    Update app.py

--- app/app.py
+++ app/app.py
@@ -147,7 +147,6 @@ def index():
     temperature, humidity, heater_status, cooler_status, humidifier_status, dehumidifier_status = incubator_app.update_values()
     current_day = incubator_app.current_day
     start_date = incubator_app.start_date
-    start_date = datetime.datetime.strptime(start_date, '%Y-%m-%d')
     return render_template("index.html", temperature=temperature, humidity=humidity, heater_status=heater_status, cooler_status=cooler_status, 
     humidifier_status=humidifier_status, dehumidifier_status=dehumidifier_status, start_date=start_date, current_day=current_day)
 
